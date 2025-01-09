336d2847f724ddd3cfb45d3ea1b908778398be07
Meghana Shastry <meghanashastry@Meghanas-MacBook-Air.local>
2025-01-09 09:59:38 -0800

    Update image structure for CR

--- apps/frontend/stories/components/categoryRow/categoryRow.stories.tsx
+++ apps/frontend/stories/components/categoryRow/categoryRow.stories.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import CategoryRow from './categoryRow'; 
-import category1 from '../../../src/images/category1.svg'; 
-import category2 from '../../../src/images/category2.svg';
-import category3 from '../../../src/images/category3.svg';
-import category4 from '../../../src/images/category4.svg'; 
-import category5 from '../../../src/images/category5.svg';
-import category6 from '../../../src/images/category6.svg';
-import category7 from '../../../src/images/category7.svg'; 
-import category8 from '../../../src/images/category8.svg';
+import category1 from '../../../src/images/categories/category1.svg'; 
+import category2 from '../../../src/images/categories/category2.svg';
+import category3 from '../../../src/images/categories/category3.svg';
+import category4 from '../../../src/images/categories/category4.svg'; 
+import category5 from '../../../src/images/categories/category5.svg';
+import category6 from '../../../src/images/categories/category6.svg';
+import category7 from '../../../src/images/categories/category7.svg'; 
+import category8 from '../../../src/images/categories/category8.svg';
 
 const meta: Meta<typeof CategoryRow> = {
   title: 'Components/CategoryRow',
